0bcfa487ffcbb929367c04582046ca540671cfbd
Philasande Valentine Nkosi <philasandenkosi8@gmail.com>
2026-09-12 04:11:32 +0200

    commit my first python project

---
+++ WelcomeScreen/script.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#For the for loop
+thistuple = ("apple", "banana", "cherry")
+
+for x in thistuple:
+    print(x)
+
+#For the for loop with range and len
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+    print(thistuple[i])
